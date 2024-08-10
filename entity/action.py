c45a28fc51335b186bea31763057c222e89b7535
Qiyi Z <qiyi.zhang@mail.utoronto.ca>
2024-08-10 12:25:01 -0400

    Remove unused directories and main.py

--- entity/action.py
+++
@@ -1,5 +0,0 @@
-class Action:
-    def __init__(self, uid1, uid2, action) -> None:
-        self.uid1 = uid1
-        self.uid2 = uid2
-        self.action = action
--- entity/interest.py
+++
@@ -1,4 +0,0 @@
-class Interest:
-    def __init__(self, uid, interest) -> None:
-        self.uid = uid
-        self.interest = interest
